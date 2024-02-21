1c45fa7c0cda162e1812ed1da0f2229eb6bd2163
aiuoki <danielceban22@gmail.com>
2024-02-21 22:50:47 +0100

    laravel

---
+++ xuxemons laravel/app/Http/Controllers/XuxemonController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Xuxemon;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class XuxemonController extends Controller
+{
+    public function index() {
+        $xuxemons = Xuxemon::all();
+        return response()->json($xuxemons, 200);
+    }
+
+    public function show($id) {
+        try {
+            $xuxemon = Xuxemon::findOrFail($id);
+            return response()->json($xuxemon, 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Xuxémon no encontrado'], 404);
+        }
+    }
+
+    public function store(Request $request) {
+        try {
+            $data = $request->validate([
+                'nombre' => 'required',
+                'tipo' => 'required|in:agua,aire,tierra',
+                'archivo' => 'required',
+            ]);
+
+            $xuxemon = Xuxemon::create($data);
+            return response()->json(['message' => 'Xuxémon creado correctamente'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Xuxémon no insertado'], 404);
+        }
+    }
+
+    public function update(Request $request, $id) {
+        try {
+            $data = $request->validate([
+                'nombre' => 'required',
+                'tipo' => 'required',
+                'archivo' => 'required',
+            ]);
+    
+            $xuxemon = Xuxemon::findOrFail($id);
+            $xuxemon->update($data);
+    
+            return response()->json(['xuxemon' => $xuxemon, 'message' => 'Xuxémon actualizado correctamente'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Xuxémon no actualizado'], 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $xuxemon = Xuxemon::findOrFail($id);
+        $xuxemon->delete();
+        return response()->json(['message' => 'Xuxémon eliminado correctamente'], 200);
+    }
+}
