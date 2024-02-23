46a5274b17651dc69e68ad73f35cc89b38b4cc24
mohamedoundouh <119254696+mohamedoundouh@users.noreply.github.com>
2024-02-23 18:43:17 +0100

    gg

--- xuxemons angular/src/app/services/xuxemon-services.service.ts
+++ xuxemons angular/src/app/services/xuxemon-services.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Xuxemon } from '../models/xuxemon.model';
 @Injectable({
   providedIn: 'root'
 })
 export class XuxemonServicesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  GetXuxemons(): Observable<any> {
+    return this.http.get(`http://127.0.0.1:8000/api/xuxemons`);
+  }
+  GetXuxemonById(id: number): Observable<any> {
+    return this.http.get(`http://127.0.0.1:8000/api/xuxemons/${id}`);
+  }
+  // UpdateXuxemonById(Xuxemon:Xuxemon , id:number): Observable<any> {
+  //   return this.http.put(`http://127.0.0.1:8000/api/xuxemons/${id}`,Xuxemon.)
+  // }
+
+
 }
