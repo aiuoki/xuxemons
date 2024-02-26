50f7c7146a6866e092b8f509b231b64bc9e5e0ca
aiuoki <danielceban22@gmail.com>
2024-02-26 20:18:19 +0100

    commit

--- xuxemons angular/src/app/components/xuxemons/create/create.component.ts
+++ xuxemons angular/src/app/components/xuxemons/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../../../services/usuario.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
+  constructor(public usuarioService: UsuarioService) { }
 
+  formLogin: FormGroup = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    tipo: new FormControl('', [Validators.required, Validators.pattern('^(aire|agua|tierra)$')]),
+    archivo: new FormControl('', [Validators.required])
+  });
+  
+  loginUsuario() {
+    const email = this.formLogin.value.email;
+    const password = this.formLogin.value.password;
+
+    this.usuarioService.loginUsuario(email, password).subscribe({
+      next: value => console.log(value),
+      error: err => alert(err)
+    });
+  }
 }
