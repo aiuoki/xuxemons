0eb11afc44ff0fb383965c9bd32fbd40ac934e18
mohamedoundouh <119254696+mohamedoundouh@users.noreply.github.com>
2024-02-26 17:49:31 +0100

    Update xuxemon-services.service.ts

--- xuxemons angular/src/app/services/xuxemon-services.service.ts
+++ xuxemons angular/src/app/services/xuxemon-services.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, catchError, of, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Xuxemon } from '../models/xuxemon.model';
 @Injectable({
   providedIn: 'root'
@@ -16,9 +15,48 @@ export class XuxemonServicesService {
   GetXuxemonById(id: number): Observable<any> {
     return this.http.get(`http://127.0.0.1:8000/api/xuxemons/${id}`);
   }
-  // UpdateXuxemonById(Xuxemon:Xuxemon , id:number): Observable<any> {
-  //   return this.http.put(`http://127.0.0.1:8000/api/xuxemons/${id}`,Xuxemon.)
-  // }
-
-
+  UpdateXuxemonById(nombre:string,tipo:string,archivo:string , id:number): Observable<any> {
+    return this.http.put(`http://127.0.0.1:8000/api/xuxemons/${id}` , {
+      nombre:nombre,
+      tipo:tipo,
+      archivo:archivo
+    }).pipe(
+    tap(() => {
+      alert("Xuxemon actualizado");
+    }),
+    catchError((err) => {
+      
+        alert("Error en el registro");
+      return of (err);
+    })
+  );
+}
+CrearXuxemon(nombre:string,tipo:string,archivo:string ): Observable<any> {
+  return this.http.put(`http://127.0.0.1:8000/api/xuxemons` , {
+    nombre:nombre,
+    tipo:tipo,
+    archivo:archivo
+  }).pipe(
+  tap(() => {
+    alert("Xuxemon creado");
+  }),
+  catchError((err) => {
+    
+      alert("Error en el registro");
+    return of (err);
+  })
+);
+}
+DeleteXuxemon(id:number ): Observable<any> {
+  return this.http.delete(`http://127.0.0.1:8000/api/xuxemons/${id}`).pipe(
+  tap(() => {
+    alert("Xuxemon borrado");
+  }),
+  catchError((err) => {
+    
+      alert("Error en el registro");
+    return of (err);
+  })
+);
+}
 }
