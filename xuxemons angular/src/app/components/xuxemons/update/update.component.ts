1907492ff43edd2572ba1f5ce0712a1389a300b6
aiuoki <danielceban22@gmail.com>
2024-02-27 12:57:42 +0100

    commit

--- xuxemons angular/src/app/components/xuxemons/update/update.component.ts
+++ xuxemons angular/src/app/components/xuxemons/update/update.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../../../services/usuario.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent {
+  constructor(public usuarioService: UsuarioService) { }
 
+  form: FormGroup = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    tipo: new FormControl('', [Validators.required, Validators.pattern('^(aire|agua|tierra)$')]),
+    archivo: new FormControl('', [Validators.required])
+  });
+
+  editarXuxemon() {
+    console.log(this.form.value);
+  }
 }
