4c7edeb5db6fcb5ef544b1731b40f9d7d544c6d8
mohamedoundouh <119254696+mohamedoundouh@users.noreply.github.com>
2024-03-06 18:48:34 +0100

    Update usuario.service.ts

--- xuxemons angular/src/app/services/usuario.service.ts
+++ xuxemons angular/src/app/services/usuario.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, of, tap } from 'rxjs';
 
+interface LoginResponse {
+  token: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
   constructor(private http: HttpClient) { }
-
+    usuarioLogueado(): boolean {
+      return localStorage.getItem('token') !== null;
+    }
   registrarUsuario(nombre:string, apellidos:string, nick:string, email: string, password: string): Observable<any> {
     return this.http.post("http://127.0.0.1:8000/api/users", {
       nombre: nombre,
       apellidos: apellidos,
       nick: nick,
-      email: email,
+      email: email, 
       password: password
     }).pipe(
       tap(() => {
@@ -31,12 +37,14 @@ export class UsuarioService {
   }
 
   loginUsuario(email: string, password: string): Observable<any> {
-    return this.http.post("http://127.0.0.1:8000/api/auth", {
+    return this.http.post<LoginResponse>("http://127.0.0.1:8000/api/auth", {
       email: email,
       password: password
     }).pipe(
-      tap(() => {
+      tap((res) => {
         alert("Usuario logueado");
+        localStorage.setItem('token', res.token);
+
       }),
       catchError((err) => {
         if (err.error && err.error.error === "Missing password") {
