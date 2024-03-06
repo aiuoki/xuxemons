5687c418db43909273362df6e73edcb0a27a3348
aiuoki <danielceban22@gmail.com>
2024-03-06 23:20:41 +0100

    Revert "Update usuario.service.ts"
    This reverts commit 4c7edeb5db6fcb5ef544b1731b40f9d7d544c6d8.

--- xuxemons angular/src/app/services/usuario.service.ts
+++ xuxemons angular/src/app/services/usuario.service.ts
@@ -2,24 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, of, tap } from 'rxjs';
 
-interface LoginResponse {
-  token: string;
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
   constructor(private http: HttpClient) { }
-    usuarioLogueado(): boolean {
-      return localStorage.getItem('token') !== null;
-    }
+
   registrarUsuario(nombre:string, apellidos:string, nick:string, email: string, password: string): Observable<any> {
     return this.http.post("http://127.0.0.1:8000/api/users", {
       nombre: nombre,
       apellidos: apellidos,
       nick: nick,
-      email: email, 
+      email: email,
       password: password
     }).pipe(
       tap(() => {
@@ -37,14 +31,12 @@ export class UsuarioService {
   }
 
   loginUsuario(email: string, password: string): Observable<any> {
-    return this.http.post<LoginResponse>("http://127.0.0.1:8000/api/auth", {
+    return this.http.post("http://127.0.0.1:8000/api/auth", {
       email: email,
       password: password
     }).pipe(
-      tap((res) => {
+      tap(() => {
         alert("Usuario logueado");
-        localStorage.setItem('token', res.token);
-
       }),
       catchError((err) => {
         if (err.error && err.error.error === "Missing password") {
