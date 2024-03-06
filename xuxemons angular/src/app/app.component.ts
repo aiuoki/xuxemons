c77a7f5976a535d7192e2b2df51078bb6d76849f
aiuoki <danielceban22@gmail.com>
2024-03-06 22:25:44 +0100

    Revert "descambios"
    This reverts commit 7c31327cc2aba5945949aa69db2445513de2a16a.

--- xuxemons angular/src/app/app.component.ts
+++ xuxemons angular/src/app/app.component.ts
@@ -9,9 +9,6 @@ import { XuxemonService } from './services/xuxemon.service';
 export class AppComponent {
   constructor(private xuxemonService: XuxemonService) { }
 
-auth = false;
-
-
   xuxemonAleatorio() {
     this.xuxemonService.xuxemonAleatorio().subscribe();
   }
