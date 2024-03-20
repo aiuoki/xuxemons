b4468a3ceff9bdcab3ead4ac8c6cd29a110bad33
aiuoki <danielceban22@gmail.com>
2024-03-20 15:14:36 +0100

    .

--- xuxemons angular/src/app/components/user/user-main/user-main.component.ts
+++ xuxemons angular/src/app/components/user/user-main/user-main.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MochilaChucheService } from 'src/app/services/mochila-chuche.service';
+import { UserXuxemonService } from 'src/app/services/user-xuxemon.service';
 
 @Component({
   selector: 'app-user-main',
@@ -6,5 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-main.component.css']
 })
 export class UserMainComponent {
+  xuxemons: any[];
+  chuches: any[];
 
+  constructor(public userXuxemonService: UserXuxemonService, public mochilaChucheService: MochilaChucheService) { }
+
+  ngOnInit() {
+    const user_id = JSON.parse(localStorage.getItem('user') || '{}').id || 0;
+    this.userXuxemonService.xuxemonsUsuario(user_id).subscribe(
+      data => {
+        this.xuxemons = Array.isArray(data.xuxemons) ? data.xuxemons : [data.xuxemons];
+        this.xuxemons = this.xuxemons.map((xuxemon, index) => ({
+          ...xuxemon,
+          archivo: `../../assets/xuxemons/${xuxemon.archivo}`,
+          tamanio: xuxemon.pivot.tamanio
+        }));
+      },
+      error => console.error(error)
+    );
+
+
+    this.mochilaChucheService.chuchesMochila(user_id).subscribe(
+      data => {
+        this.chuches = Array.isArray(data.chuches) ? data.chuches : [data.chuches];
+        this.chuches = this.chuches.map((chuche, index) => ({
+          ...chuche,
+          archivo: `../../assets/chuches/${chuche.archivo}`,
+          cantidad: chuche.pivot.cantidad
+        }));
+      },
+      error => console.error(error)
+    );
+  }
+
+  form: FormGroup = new FormGroup({
+    id_xuxemon: new FormControl('', Validators.required),
+    id_chuche: new FormControl('', Validators.required)
+  });
+
+  alimentarXuxemon() {
+    const id_xuxemon = this.form.value.id_xuxemon;
+    const id_chuche = this.form.value.id_chuche;
+
+    this.userXuxemonService.alimentarXuxemonUsuario(id_chuche, id_xuxemon).subscribe({
+      next: (response) => {
+        alert('Xuxemon alimentado!');
+        this.ngOnInit();
+      },
+      error: (error) => {
+        alert('Error al alimentar el xuxemon.');
+      }
+    });
+  }
 }
--- xuxemons angular/src/app/components/user/xuxedex/xuxedex.component.ts
+++ xuxemons angular/src/app/components/user/xuxedex/xuxedex.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MochilaChucheService } from 'src/app/services/mochila-chuche.service';
 import { UserXuxemonService } from 'src/app/services/user-xuxemon.service';
 
 @Component({
@@ -10,9 +8,8 @@ import { UserXuxemonService } from 'src/app/services/user-xuxemon.service';
 })
 export class XuxedexComponent {
   xuxemons: any[];
-  chuches: any[];
 
-  constructor(public userXuxemonService: UserXuxemonService, public mochilaChucheService: MochilaChucheService) { }
+  constructor(public userXuxemonService: UserXuxemonService) { }
 
   ngOnInit() {
     const user_id = JSON.parse(localStorage.getItem('user') || '{}').id || 0;
@@ -28,39 +25,5 @@ export class XuxedexComponent {
       },
       error => console.error(error)
     );
-
-    // ------------
-    this.mochilaChucheService.chuchesMochila(user_id).subscribe(
-      data => {
-        this.chuches = Array.isArray(data.chuches) ? data.chuches : [data.chuches];
-        this.chuches = this.chuches.map((chuche, index) => ({
-          ...chuche,
-          archivo: `../../assets/chuches/${chuche.archivo}`,
-          cantidad: chuche.pivot.cantidad
-        }));
-      },
-      error => console.error(error)
-    );
-  }
-
-  form: FormGroup = new FormGroup({
-    id_xuxemon: new FormControl('', Validators.required),
-    id_chuche: new FormControl('', Validators.required)
-  });
-
-  alimentarXuxemon() {
-    const id_xuxemon = this.form.value.id_xuxemon;
-    const id_chuche = this.form.value.id_chuche;
-    console.log(id_xuxemon, id_chuche);
-
-    this.userXuxemonService.alimentarXuxemonUsuario(id_chuche, id_xuxemon).subscribe({
-      next: (response) => {
-        alert('Xuxemon alimentado!');
-        this.ngOnInit();
-      },
-      error: (error) => {
-        alert('Error al alimentar el xuxemon.');
-      }
-    });
   }
 }
